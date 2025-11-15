f6290686022feab38f3616dbe88be213fa5bbdbc
Your Name <godclutch958@gmail.com>
2025-11-15 13:25:20 +0400

    favicon and title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HomeMain from "@/pages/homes/home/HomeMain";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Home - Agntix Digital Agency & Creative Portfolio NextJs Template",
+  title: "A R K Packaging Industries L.L.C.",
 };
 
 export default function Home() {
